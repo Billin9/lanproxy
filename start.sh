1d06347f422187583f30345638c9f1b5f994de09
Billin9 <Billin9@hotmail.com>
2019-08-05 22:08:31 +0800

    删除 rm 参数

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -d --name lanproxy --net host hub.dosec.cn/library/lanproxy:2019-08-05T18.32.49
+docker run -d --name lanproxy --net host hub.dosec.cn/library/lanproxy:2019-08-05T18.32.49
